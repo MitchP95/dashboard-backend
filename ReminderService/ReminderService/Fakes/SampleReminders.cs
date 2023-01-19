0531da52994a1726aa90eb54008e3b6b398c8b59
Mitchell Puersten <mitch.puersten@gmail.com>
2023-01-19 07:52:35 -0500

    Populate reminder samples.

--- ReminderService/ReminderService/Fakes/SampleReminders.cs
+++ ReminderService/ReminderService/Fakes/SampleReminders.cs
@@ -1,6 +1,28 @@
 ﻿namespace ReminderService.Fakes
 {
-    public class SampleReminders
+    public static class SampleReminders
     {
+        public static List<Reminder> GetSampleReminders ()
+        {
+            List<Reminder> reminders= new List<Reminder> ();
+
+            reminders.Add(new Reminder()
+            {
+                Contributor = "Bob Kanakis",
+                Description = "Pick a new first name for myself",
+                DateTimeAdded= DateTime.Now,
+                IsComplete= false,
+            });
+
+            reminders.Add(new Reminder()
+            {
+                Contributor = "Margret Kanakis",
+                Description = "Help Bob pick a new name",
+                DateTimeAdded = DateTime.Now,
+                IsComplete = false,
+            });
+
+            return reminders;
+        }
     }
 }
