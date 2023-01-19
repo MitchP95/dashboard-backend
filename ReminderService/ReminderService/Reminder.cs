e84b3e73799c20a85b5fecf41970f4b850a0e195
Mitchell Puersten <mitch.puersten@gmail.com>
2023-01-19 07:31:49 -0500

    Added Reminder data model and removed .vs items from commit.

---
+++ ReminderService/ReminderService/Reminder.cs
@@ -0,0 +1,15 @@
+﻿namespace ReminderService
+{
+    public class Reminder
+    {
+        public string Contributor { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateTimeAdded { get; set; }
+
+        public DateTime? DateTimeCompleted { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
