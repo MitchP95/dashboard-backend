8fdc4a14d4d80417c21e0d7ebdd3725dc8781e8f
Mitchell Puersten <mitch.puersten@gmail.com>
2023-01-23 07:15:03 -0500

    Added built-in reminder controller to do NV work with reminders.

---
+++ ReminderService/ReminderService/Controllers/BuiltInReminderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReminderService.Fakes;
+
+namespace ReminderService.Controllers
+{
+    [ApiController]
+    [Route("[builtInReminderController]")]
+    public class BuiltInReminderController : ControllerBase
+    {
+        [HttpGet(Name = "GetReminders")]
+        public IEnumerable<Reminder> Get()
+        {
+            return SampleReminders.GetSampleReminders();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Reminder reminder)
+        {
+            return NoContent();
+        }
+    }
+}
